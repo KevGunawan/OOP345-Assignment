77548f1657343e6a75909bd98a21c5aee9ab60fc
KevGunawan <142485933+KevGunawan@users.noreply.github.com>
2023-11-07 19:54:41 -0500

    update ms1

--- MS1/Station.cpp
+++ MS1/Station.cpp
@@ -23,13 +23,20 @@ namespace sdds {
 		Utilities utils;
 		bool more = true;
 		size_t pos = 0;
+		auto whiteSpace = [](const std::string str) {
+			size_t start = str.find_first_not_of(" \t");
+			size_t end = str.find_last_not_of(" \t");
+			return str.substr(start, end - start + 1);
+		};
 
 		try {
 			m_name = utils.extractToken(line, pos, more);
+			m_name = whiteSpace(m_name);
 			m_serial_num = std::stoi(utils.extractToken(line, pos, more));
 			m_curr_items = std::stoi(utils.extractToken(line, pos, more));
 			Station::m_widthField = std::max(utils.getFieldWidth(), Station::m_widthField);
 			m_desc = utils.extractToken(line, pos, more);
+			m_desc = whiteSpace(m_desc);
 		}
 		catch (std::string err) {
 			std::cout << err;
@@ -66,10 +73,10 @@ namespace sdds {
 	{
 		os << std::right << std::setw(3) << std::setfill('0') << m_id << " | ";
 		os << std::left << std::setw(Station::m_widthField) << std::setfill(' ') << m_name;
-		os << " | " << std::right << std::setw(6) << std::setfill('0') << m_serial_num << " |";
+		os << "| " << std::right << std::setw(6) << std::setfill('0') << m_serial_num << " | ";
 
 		if (full) {
-			os << std::right << std::setw(4) << std::setfill(' ') << m_curr_items <<"|";
+			os << std::right << std::setw(4) << std::setfill(' ') << m_curr_items <<" | ";
 			os << std::left << m_desc << std::endl;
 		}
 		else{
